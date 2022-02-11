ad045bb02360b347d3a7601f5d4300af57f5875a
Umair Ahmed <umair.ahmad1257@gmail.com>
2022-02-11 21:25:07 +0500

    Adding Label options for customer portal

--- template-parts/customer/entry-invoice.php
+++ template-parts/customer/entry-invoice.php
@@ -1,11 +1,11 @@
 <?php ?>
 <div class="invoice__upload-form">
-    <h3><?php _e( 'Upload your invoice', 'vrchecke' );?></h3>
+    <h3><?php _e(get_theme_mod('company_form_upload_heading', 'Upload your invoice'), 'vrchecke');?></h3>
     <form action="" method="post" enctype="multipart/form-data">
         <div class="portal__invoice-wrap portal__input-wrap">
             <input type="file" class="invoice portal__input" name="invoice" />
-            <input type="submit" value="Submit">
-            <input type="hidden" name="invoice_nonce" value="<?php echo wp_create_nonce( 'invoice_nonce_action' ); ?>">
+            <input type="submit" value="<?php _e(get_theme_mod('company_form_upload_button', 'Submit'), 'vrchecke');?>">
+            <input type="hidden" name="invoice_nonce" value="<?php echo wp_create_nonce('invoice_nonce_action'); ?>">
         </div>
     </form>
 </div>
